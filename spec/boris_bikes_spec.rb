8464b370813372919e9013967b71a670e0ff1969
Johnny Dworzynski <j.dworzynski@me.com>
2016-08-16 12:30:56 +0100

    added 1st rspec check if release_bike method exists

--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -2,4 +2,8 @@ require "boris_bikes"
 
 describe DockingStation do
 
+  it 'expects DockingStation to respond to release_bike method' do
+      expect(DockingStation).to respond_to(release_bike)
+  end
+
 end
